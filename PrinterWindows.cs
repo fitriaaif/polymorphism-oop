9cb85e4f5754d69bfac813926a581e37358df736
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-06-12 20:24:08 +0700

    polymorphism

---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer display dimension");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer printing....");
+        }
+    }
+}
